7f2febf5c0bf9c2093298906f2c50e47a83ddc61
Calista <calistasalsabila313@gmail.com>
2025-05-27 11:20:36 +0700

    add logical operator

--- Dart/code/dart_basic/bin/11_logical_operator.dart
+++ Dart/code/dart_basic/bin/11_logical_operator.dart
@@ -3,5 +3,19 @@ void main() {
   var presence = 15;
 
   var ipKeren = ip > 3.5;
-  var minPresence = ip 
+  var minPresence = presence > 12;
+
+  print(ipKeren); // true
+  print(minPresence); // true
+
+  var gudJob = ipKeren && minPresence; // both must be true
+  print(gudJob); //true
+
+  // logical or
+  var gudJob2 = ipKeren || minPresence; // at least one must be true
+  print(gudJob2); // true
+
+  // logical not
+  var notGudJob = !gudJob; // negates the value
+  print(notGudJob); // false
 }
